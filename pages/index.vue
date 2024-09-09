7ab5635e391503d4a6e1ac7d9037fa3cce61e0b6
sugengw <sugeng.warsito@transtrack.id>
2024-09-09 13:24:58 +0700

    feat: init tailwind

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="border h-screen flex items-center justify-center">
+    <div>
+      <p class="text-4xl font-semibold">Nuxt Auth Utils</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
